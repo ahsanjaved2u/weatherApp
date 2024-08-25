572fcf43d7e844a88298036f19ed3b4aeaaefaed
ahsan javed <ahsanjaved2u@gmail.com>
2024-08-26 00:34:20 +0500

    Fix import environment variable  errors and update requirements..

--- App.py
+++ App.py
@@ -38,9 +38,9 @@ driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
 try:
     with driver.session() as session:
         result = session.run("RETURN 1")
-        print("Connection successful!")
+        st.write("Connection successful!")
 except Exception as e:
-    print(f"Connection failed: {e}")
+    st.write(f"Connection failed: {e}")
 
 
 
